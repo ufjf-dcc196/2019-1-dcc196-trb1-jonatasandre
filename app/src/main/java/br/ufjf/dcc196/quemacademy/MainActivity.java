7481a0d3ed7ad4fde0e8f2610fd92f1ee5f9b0ff
Jônatas André <jjsfandre@gmail.com>
2019-05-01 18:38:49 -0300

    Configurando MainActivity como tela de boas vindas

--- app/src/main/java/br/ufjf/dcc196/quemacademy/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/quemacademy/MainActivity.java
@@ -1,6 +1,7 @@
 package br.ufjf.dcc196.quemacademy;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,28 +25,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		
-		Button btnNovoPlanejamento = findViewById(R.id.btnNovoPlanejamento);
-		btnNovoPlanejamento.setOnClickListener(new View.OnClickListener(){
 
-			@Override
-			public void onClick(View v){
-				Intent intent = new Intent(MainActivity.this, NovoPlanejamentoActivity.class);
-				startActivity(intent);
-			}
-		});
-
-		/*
-        final RecyclerView rv = findViewById(R.id.rvPalavras);
-
-        PalavraAdapter pAdapter = new PalavraAdapter(this.palavras);
-        pAdapter.setOnPalavraClickListener(new PalavraAdapter.OnPalavraClickListener() {
-            @Override
-            public void onItemClick(View itemView, int position) {
-                Toast.makeText(MainActivity.this, palavras.get(position), Toast.LENGTH_SHORT).show();
-            }
-        });
-        rv.setAdapter(pAdapter);
-        rv.setLayoutManager(new LinearLayoutManager(this));*/
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(MainActivity.this, PlanejamentosActivity.class);
+                                    startActivity(intent);
+                                }
+                            },5000);
     }
 }
