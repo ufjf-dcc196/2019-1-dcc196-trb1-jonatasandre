fdf2edb099d0a6b16b5df2c6fb2a3c4e1b28a7b7
Jônatas André <jjsfandre@gmail.com>
2019-05-02 00:30:45 -0300

    Incluindo método para criar dados iniciais

--- app/src/main/java/br/ufjf/dcc196/quemacademy/PlanejamentosActivity.java
+++ app/src/main/java/br/ufjf/dcc196/quemacademy/PlanejamentosActivity.java
@@ -9,24 +9,45 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlanejamentosActivity extends AppCompatActivity {
     public static final int REQUEST_NOVO_PLANEJAMENTO = 100;
+    public static final int REQUEST_DISCIPLINAS = 200;
 
-    public List<Planejamento> planejamentosList = new ArrayList<Planejamento>() {{
-        add(new Planejamento(2017, 1, 20, 20, 20, 40));
-        add(new Planejamento(2017, 2, 10, 20, 30, 40));
-    }};
+    public List<Planejamento> planejamentosList = new ArrayList<Planejamento>();
     PlanejamentosAdapter pAdapter;
+
+    private void criaDadosInicias(){
+        Planejamento p = new Planejamento(2017, 1, 20, 20, 20, 40);
+        Disciplina d1 = new Disciplina("Disciplina 1",20,"Línguas");
+        Disciplina d2 = new Disciplina("Disciplina 2",30,"Exatas");
+        Disciplina d3 = new Disciplina("Disciplina 3",40,"Humanidades");
+        Disciplina d4 = new Disciplina("Disciplina 3",50,"Saúde");
+        p.addDisciplina(d1);
+        p.addDisciplina(d2);
+        p.addDisciplina(d3);
+        p.addDisciplina(d4);
+        Planejamento p2 = new Planejamento(2017, 2, 10, 30, 40, 20);
+        Disciplina d5 = new Disciplina("Disciplina 5",10,"Línguas");
+        Disciplina d6 = new Disciplina("Disciplina 6",20,"Exatas");
+        Disciplina d7 = new Disciplina("Disciplina 7",30,"Humanidades");
+        Disciplina d8 = new Disciplina("Disciplina 8",40,"Saúde");
+        p2.addDisciplina(d5);
+        p2.addDisciplina(d6);
+        p2.addDisciplina(d7);
+        p2.addDisciplina(d8);
+        planejamentosList.add(p);
+        planejamentosList.add(p2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planejamentos);
+        criaDadosInicias();
 
         Button btnNovoPlanejamento = findViewById(R.id.btnNovoPlanejamento);
 		btnNovoPlanejamento.setOnClickListener(new View.OnClickListener(){
